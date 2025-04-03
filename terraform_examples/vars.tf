8017758808a91c5c949ea18ab0c3314f66f69f57
Viral Patel <32780999+routerworld3@users.noreply.github.com>
2025-04-03 08:49:11 -0400

    Update vars.tf

--- terraform_examples/vars.tf
+++ terraform_examples/vars.tf
@@ -1,46 +1,42 @@
-resource "aws_lb" "web" {
-  name               = var.alb_name
-  internal           = false
-  load_balancer_type = "application"
-  subnets            = var.public_subnet_ids
-  security_groups    = [var.alb_sg_id]
+
+variable "region" {
+  description = "AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
 }
 
-resource "aws_lb_target_group" "s3_endpoint" {
-  name        = "s3-endpoint-tg"
-  port        = 443
-  protocol    = "HTTPS"
-  target_type = "ip"
-  vpc_id      = var.vpc_id
-
-  health_check {
-    path                = "/"
-    protocol            = "HTTPS"
-    matcher             = "200"
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.20.0.0/16"
 }
 
-resource "aws_lb_listener" "https" {
-  load_balancer_arn = aws_lb.web.arn
-  port              = 443
-  protocol          = "HTTPS"
-  certificate_arn   = var.acm_certificate_arn
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.s3_endpoint.arn
-  }
+variable "public_subnet_cidrs" {
+  description = "List of CIDRs for public subnets"
+  type        = list(string)
+  default     = ["10.20.0.0/24", "10.20.1.0/24"]
 }
 
-resource "aws_vpc_endpoint" "s3_interface" {
-  vpc_id             = var.vpc_id
-  service_name       = "com.amazonaws.${var.region}.s3"
-  vpc_endpoint_type  = "Interface"
-  subnet_ids         = var.private_subnet_ids
-  security_group_ids = [var.vpce_sg_id]
-  private_dns_enabled = true
+variable "private_subnet_cidrs" {
+  description = "List of CIDRs for private subnets"
+  type        = list(string)
+  default     = ["10.20.100.0/24", "10.20.101.0/24"]
+}
+
+variable "availability_zones" {
+  description = "Availability zones for the subnets"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
+variable "alb_name" {
+  description = "Name of the ALB"
+  type        = string
+  default     = "web-alb"
+}
+
+variable "acm_certificate_arn" {
+  description = "ARN of the ACM certificate for HTTPS listener"
+  type        = string
+  default     = "arn:aws:acm:region:account:certificate/your-cert-id"
 }
